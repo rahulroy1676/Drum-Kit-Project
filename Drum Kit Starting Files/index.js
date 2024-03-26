66e23725a13b4ff5539db427d794817440da6d0f
rroy-viewrail <148786746+rroy-viewrail@users.noreply.github.com>
2024-03-26 10:13:34 -0400

    Final commit

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,10 +1,30 @@
 var drumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < drumButtons; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-        var buttonInnerHTML = this.innerHTML
 
-        switch (buttonInnerHTML) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+
+        var buttonInnerHTML = this.innerHTML;
+
+        makeSound(buttonInnerHTML);
+
+        buttonAnimation(buttonInnerHTML);
+
+        });
+
+    }
+    
+        
+    document.addEventListener("keypress", function(event){
+        makeSound(event.key);
+
+    buttonAnimation(event.key);
+
+    });
+
+        function makeSound(key) {
+
+        switch (key) {
             case "w":
                 var tom1 = new Audio("./sounds/tom-1.mp3");
                 tom1.play();
@@ -36,10 +56,17 @@ for (var i = 0; i < drumButtons; i++) {
         
             default:
                 console.log(buttonInnerHTML);
-                break;
         }
+    }
         // var audio = new Audio('./sounds/tom-2.mp3');
         // audio.play();
-    });
-}
+
+    function buttonAnimation(currentKey){
+        var activeButton = document.querySelector("." + currentKey);
+        activeButton.classList.add("pressed");
+
+        setTimeout(function() {
+            activeButton.classList.remove("pressed");
+        }, 100);
+    }
 
